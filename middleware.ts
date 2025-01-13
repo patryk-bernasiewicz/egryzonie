ca1cbdace64f690223681d225d712d362ae921e5
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-01-13 22:50:58 +0100

    handle redirection from home page to vets

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
-import createMiddleware from "next-intl/middleware";
+import createMiddleware from 'next-intl/middleware';
 
-import { routing } from "./i18n/routing";
+import { routing } from './i18n/routing';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/' || pathname === '/home') {
+    return NextResponse.redirect(new URL('/weterynarze', request.url));
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
